3e735b990c9328c1c4b498945fe30c1fc6f85851
Akho10 <APhungula@sars.gov.za>
2026-09-07 15:42:16 +0200

    Added add to cart transactional method

---
+++ sportyshoes/src/main/java/com/sportyshoes/service/CartService.java
@@ -0,0 +1,54 @@
+package com.sportyshoes.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sportyshoes.model.Cart;
+import com.sportyshoes.model.Product;
+import com.sportyshoes.repository.CartRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class CartService {
+
+	@Autowired
+	CartRepository cartRepository;
+	
+	@Autowired
+	ProductService productService;
+	
+	//getCartByUser
+	@Transactional
+	public Cart addToCart(long productId,long userId, int quantity) throws Exception {
+		//find user cart
+		 Cart cart = cartRepository.findCartByUserId(userId);
+		 
+		 if(cart == null) {
+			 throw new Exception("Cart is empty");
+		 }
+		 //find product
+		Product product = productService.findProductById(productId);
+		 //quantity
+		if(quantity <= 0 ) {
+			throw new Exception("Quantity must be greater than 0");
+		}
+		if(quantity > product.getQuantity()) {
+			
+//		product.setQuantity(product.getQuantity());
+			throw new Exception("Not enough stock");
+		
+		}
+		//add product to cart
+		cart.getProducts().add(product);
+		//total is prod price * quantity
+		cart.setTotalAmount(cart.getTotalAmount() + (product.getPrice() * quantity));
+		
+		return cartRepository.save(cart);
+	
+		 
+	}
+	//getCartByUser
+}
