7048a89a87ea7e33ddc279f10d1a29b06cb0b462
Akho10 <APhungula@sars.gov.za>
2026-09-07 15:42:54 +0200

    Added restApi endpoint for add to cart

---
+++ sportyshoes/src/main/java/com/sportyshoes/controller/CartController.java
@@ -0,0 +1,24 @@
+package com.sportyshoes.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sportyshoes.model.Cart;
+import com.sportyshoes.service.CartService;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+	@Autowired
+	CartService cartService;
+	
+	@PostMapping("add/{userId}/{productId}/{quantity}")
+	public String addToCart(@PathVariable long productId,@PathVariable long userId, @PathVariable int quantity) throws Exception{
+		cartService.addToCart(productId, userId, quantity);
+		return "Product added to cart";
+	}
+}
